6e21f6e9503ea2ba65f6d89a42725708a3e1cc0e
hectormv11 <h.montoya.v02@gmail.com>
2024-02-26 10:30:24 +0100

    recursividad

--- Recursividad/ejercicios/recursividad.java
+++ Recursividad/ejercicios/recursividad.java
@@ -13,9 +13,22 @@ public class recursividad {
 		//System.out.println(potencias(3,3));
 
 		//invertir(123);
-		
+
 		//System.out.println(palabraOrdenada("1234"));
-		System.out.println(palindromo("radar"));
+		//System.out.println(palindromo("radar"));
+
+		/*System.out.println(binario(0));
+		System.out.println(binario(1));
+		System.out.println(binario(4));
+		System.out.println(binario(6));
+		System.out.println(binario(27));
+		System.out.println(binario(274));*/
+
+		/*System.out.println(comprobarBinario(1001012));
+		System.out.println(comprobarBinario(1));*/
+
+		/*System.out.println(cerciente(123465));
+		System.out.println(decreciente(123465));*/
 	}
 
 	public static int sumaN(int n) {
@@ -93,29 +106,68 @@ public class recursividad {
 	public static boolean palabraOrdenada(String str) {
 
 		if (str.length() <= 1) {
-            return true;
-        } else {
-            if (str.charAt(0) <= str.charAt(1)) {
-                return palabraOrdenada(str.substring(1));
-            } else {
-                return false;
-            }
-        }
-		
+			return true;
+		} else {
+			if (str.charAt(0) <= str.charAt(1)) {
+				return palabraOrdenada(str.substring(1));
+			} else {
+				return false;
+			}
+		}
+
 	}
-	
+
 	public static boolean palindromo(String str) {
 
 		if (str.length() == 1 || str.length() == 0) {
-            return true;
-        } else {
-            if (str.charAt(0) == str.charAt(str.length()-1)) {
-                return palindromo(str.substring(1,str.length()-1));
-            } else {
-                return false;
-            }
-        }
+			return true;
+		} else {
+			if (str.charAt(0) == str.charAt(str.length()-1)) {
+				return palindromo(str.substring(1,str.length()-1));
+			} else {
+				return false;
+			}
+		}
+
+	}
+
+	public static String binario(int n) {
+
+		if(n == 1 || n == 0)return "" + n;
+
+		return binario(n/2) + n%2;
+
+	}
+
+	public static boolean comprobarBinario(int n) {
+
+		if(n == 0)return true;
+
+		if (n%10 == 0 || n%10 == 1) {
+			return comprobarBinario(n/10);
+		} else {
+			return false;
+		}
+
+
+	}
+
+	/*public static boolean cerciente(int n) {
+
+		if(n < 10) {
+			return true;
+		}
+		
+		if(Integer.toString(n)) {
+			
+		}
 		
 	}
+
+	public static boolean decreciente(int n) {
+		
+		
+		
+	}*/
 
 }
